73c34811dec02bf60ffea339c49620e69a327dfa
Degisiklikler1 <megaozi9517536@gmail.com>
2024-04-13 14:47:50 +0300

    registerButton, firstname, lastname locater'ları eklendi

--- src/test/java/Pages/DialogContent_Oguzhan.java
+++ src/test/java/Pages/DialogContent_Oguzhan.java
@@ -1,7 +1,21 @@
 package Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class DialogContent_Oguzhan extends ParentPage{
 
-
+    @FindBy(id = "firstname")
+    public WebElement firstname;
+    @FindBy(xpath = "lastname")
+    public WebElement lastname;
+    @FindBy(xpath = "//ul[@class='header links']//li[3]")
+    public WebElement email;
+    @FindBy(xpath = "//ul[@class='header links']//li[3]")
+    public WebElement password;
+    @FindBy(xpath = "//ul[@class='header links']//li[3]")
+    public WebElement confirmPassword;
+    @FindBy(xpath = "//ul[@class='header links']//li[3]")
+    public WebElement createAnAccountButton;
 
 }
--- src/test/java/Pages/LeftNav_Oguzhan.java
+++ src/test/java/Pages/LeftNav_Oguzhan.java
@@ -1,6 +1,12 @@
 package Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LeftNav_Oguzhan extends ParentPage {
+
+    @FindBy(xpath = "//ul[@class='header links']//li[3]")
+    public WebElement registerButton;
 
 
 
