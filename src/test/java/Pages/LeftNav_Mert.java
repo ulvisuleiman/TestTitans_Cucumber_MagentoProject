8c471a901b6c0b840325d777a039fbf166e74f5b
MertOzmen <avmertozmen93@gmail.com>
2024-04-15 02:12:10 +0300

    Some locators were found and 2 steps were create

--- src/test/java/Pages/LeftNav_Mert.java
+++ src/test/java/Pages/LeftNav_Mert.java
@@ -1,8 +1,16 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LeftNav_Mert extends ParentPage{
     public LeftNav_Mert() {PageFactory.initElements(GWD.getDriver(),this);}
+
+    @FindBy (xpath = "(//li[@class='authorization-link']/a)[2]")
+    public WebElement SignInButton;
+
+    @FindBy (xpath = "(//button[@id='send2']/span)[1]")
+    public WebElement LoginButton;
 }
--- src/test/java/StepDefinitions/US_702_MertSteps.java
+++ src/test/java/StepDefinitions/US_702_MertSteps.java
@@ -1,16 +1,20 @@
 package StepDefinitions;
 
+import Pages.DialogContent_Mert;
+import Pages.LeftNav_Mert;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class US_702_MertSteps {
+    LeftNav_Mert lfm=new LeftNav_Mert();
+    DialogContent_Mert dcm=new DialogContent_Mert();
     @When("Click on the Sign In button")
     public void clickOnTheSignInButton() {
-
-
+        lfm.myClick(lfm.SignInButton);
     }
 
     @Then("Log in without filling in username and password")
     public void logInWithoutFillingInUsernameAndPassword() {
+        lfm.myClick(lfm.LoginButton);
     }
 }
