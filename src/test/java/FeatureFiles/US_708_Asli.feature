9e9a9e123823089b17702f7d69a396057ed107c9
asli003 <z.aslicelik@gmail.com>
2024-04-14 20:43:42 +0300

    Asli değişiklikler 1

---
+++ src/test/java/FeatureFiles/US_708_Asli.feature
@@ -0,0 +1,3 @@
+Feature: Add to  Wish List
+
+
--- src/test/java/Pages/DialogContent_Asli.java
+++ src/test/java/Pages/DialogContent_Asli.java
@@ -1,4 +1,37 @@
 package Pages;
 
-public class DialogContent_Asli {
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DialogContent_Asli extends ParentPage {
+
+
+    public DialogContent_Asli(){
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+        @FindBy(xpath = "(//a[contains(text(),'Sign In')])[1]")
+    public WebElement sign;
+
+    @FindBy(css = "[name='login[username]']")
+    public WebElement email;
+
+    @FindBy(css = "[name='login[password]']")
+        public WebElement password;
+
+    @FindBy(xpath = "(//button[@name='send'])[1]")
+    public WebElement buttonSign2;
+
+
+
+
+
+
+
+
+
+
+
 }
