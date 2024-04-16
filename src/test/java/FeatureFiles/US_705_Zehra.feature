5d7d8a0846b2a26198e14eb4e2448522575ad8ed
ZGK <zehragkrl@gmail.com>
2024-04-17 00:36:09 +0300

    updated the methods

--- src/test/java/FeatureFiles/US_705_Zehra.feature
+++ src/test/java/FeatureFiles/US_705_Zehra.feature
@@ -2,9 +2,8 @@ Feature: Adding and removing items from cart
 
   Background:
     Given Navigate to Magento Website
-    When Click on the Sign In button
-    Then Finally, user must login to the account
-    And User should see successful login message
+    When User must login to the account
+
 
     Scenario: User should select an item and delete it
       When Click on random category from Tab Menu
--- src/test/java/StepDefinitions/US_705_ZehraSteps.java
+++ src/test/java/StepDefinitions/US_705_ZehraSteps.java
@@ -1,18 +1,31 @@
 package StepDefinitions;
 
 import Pages.DialogContent_Zehra;
+import Utilities.GWD;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US_705_ZehraSteps {
     DialogContent_Zehra dc=new DialogContent_Zehra();
+    Actions actions=new Actions(GWD.getDriver());
+    public WebDriverWait wait=new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(7));
     @When("Click on random category from Tab Menu")
     public void clickOnRandomCategoryFromTabMenu() {
-        dc.myClick(dc.womenTab);
-        dc.myClick(dc.bottomsTab);
-        dc.myClick(dc.shortsTab);
+        actions.moveToElement(dc.womenTab).build().perform();
+        wait.until(ExpectedConditions.visibilityOf(dc.bottomsTab));
+        actions.moveToElement(dc.bottomsTab).perform();
+        //dc.myClick(dc.womenTab);
+        //new Actions(GWD.getDriver()).moveToElement(dc.bottomsTab).build().perform();
+
+        //dc.myClick(dc.bottomsTab);
+        dc.myJSClick(dc.shortsTab);
     }
 
     @Then("Add random item from the list")
