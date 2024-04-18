be9bfd1c0a2b079b4e9663ccb0d8993d6b9f86bc
Ulvi <ulvisuleiman@gmail.com>
2024-04-18 19:20:47 +0400

    US_706 tamamlandi fakat bazi ufak locator hatalari var. Giderilecek.

---
+++ src/test/resources/StepDefinitions/MyStepdefs.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("User selects a product from the items")
+    public void userSelectsAProductFromTheItems() {
+    }
+
+    @When("User adds the product to the cart and completes the order")
+    public void userAddsTheProductToTheCartAndCompletesTheOrder() {
+    }
+
+    @Then("User should see successfull payment message")
+    public void userShouldSeeSuccessfullPaymentMessage() {
+    }
+}
